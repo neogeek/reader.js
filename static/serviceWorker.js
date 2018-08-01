78b18c775e6091bd8e3c5bca7c2604edb959eb56
Scott Doxey <neogeek0101@gmail.com>
2018-08-01 17:44:52 -0400

    Whitespace.

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -9,19 +9,13 @@ const cachedFiles = [
     '/js/app.min.js'
 ];
 
-const networkFiles = [
-    '/feed',
-    'https://www.googleapis.com/'
-];
+const networkFiles = ['/feed', 'https://www.googleapis.com/'];
 
 self.addEventListener('install', event => {
 
     console.log('[pwa install]');
 
-    event.waitUntil(
-        caches.open(cacheVersion)
-            .then(cache => cache.addAll(cachedFiles))
-    );
+    event.waitUntil(caches.open(cacheVersion).then(cache => cache.addAll(cachedFiles)));
 
 });
 
@@ -30,12 +24,15 @@ self.addEventListener('activate', event => {
     console.log('[pwa activate]');
 
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.filter(key => key.indexOf(cacheName) === 0 && key !== cacheVersion)
-                    .map(key => caches.delete(key))
+        caches
+            .keys()
+            .then(keys =>
+                Promise.all(
+                    keys
+                        .filter(key => key.indexOf(cacheName) === 0 && key !== cacheVersion)
+                        .map(key => caches.delete(key))
+                )
             )
-        )
     );
 
     return self.clients.claim();
@@ -49,8 +46,7 @@ self.addEventListener('fetch', event => {
         console.log('[network fetch]', event.request.url);
 
         event.respondWith(
-            caches.match(event.request)
-                .then(response => response || fetch(event.request))
+            caches.match(event.request).then(response => response || fetch(event.request))
         );
 
     } else {
@@ -58,14 +54,13 @@ self.addEventListener('fetch', event => {
         console.log('[pwa fetch]', event.request.url);
 
         event.respondWith(
-            caches.match(event.request)
-                .then(response => {
+            caches.match(event.request).then(response => {
 
-                    caches.open(cacheVersion).then(cache => cache.add(event.request.url));
+                caches.open(cacheVersion).then(cache => cache.add(event.request.url));
 
-                    return response || fetch(event.request);
+                return response || fetch(event.request);
 
-                })
+            })
         );
 
     }
