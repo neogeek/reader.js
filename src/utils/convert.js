f3224b249489f478d2db9e8bd419c1d1322c26e1
neogeek <neogeek0101@gmail.com>
2019-05-28 23:02:20 -0400

    Added missing nav toggle css class name.
    Whitespace.

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -2,24 +2,18 @@ const url = require('url');
 
 const crypto = require('crypto');
 
-const {parseString} = require('xml2js');
+const { parseString } = require('xml2js');
 
 const convertFeedFromXMLtoJSON = response =>
     new Promise((resolve, reject) => {
-
         parseString(response.body, {}, (err, result) => {
-
             if (err) {
-
                 return reject(err);
-
             }
 
             if (result.feed && result.feed.entry) {
-
                 resolve(
                     result.feed.entry.map(entry => {
-
                         const link = entry.link[0].$.href;
 
                         const hash = crypto
@@ -30,19 +24,19 @@ const convertFeedFromXMLtoJSON = response =>
                         return {
                             hash,
                             link,
-                            'origin': response.request.uri.href,
-                            'site': url.parse(link).hostname,
-                            'title': entry.title[0]
+                            origin: response.request.uri.href,
+                            site: url.parse(link).hostname,
+                            title: entry.title[0]
                         };
-
                     })
                 );
-
-            } else if (result.rss && result.rss.channel && result.rss.channel[0].item) {
-
+            } else if (
+                result.rss &&
+                result.rss.channel &&
+                result.rss.channel[0].item
+            ) {
                 resolve(
                     result.rss.channel[0].item.map(item => {
-
                         const [link] = item.link;
 
                         const hash = crypto
@@ -53,20 +47,16 @@ const convertFeedFromXMLtoJSON = response =>
                         return {
                             hash,
                             link,
-                            'origin': response.request.uri.href,
-                            'site': url.parse(link).hostname,
-                            'title': item.title[0]
+                            origin: response.request.uri.href,
+                            site: url.parse(link).hostname,
+                            title: item.title[0]
                         };
-
                     })
                 );
-
             }
 
             return reject(new Error('Invalid XML/RSS feed.'));
-
         });
-
     });
 
 module.exports = {
